9e68f95a9c9de3748a8d1b908711d4c4c5fde4d4
hamin07 <haminyang2@naver.com>
2025-01-13 18:28:05 +0900

    commit

---
+++ p/src/p/Test01.java
@@ -0,0 +1,10 @@
+package p;
+
+import java.util.Scanner;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World");
+	}
+}
