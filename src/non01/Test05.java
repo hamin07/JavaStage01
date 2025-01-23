b67390f18f59053c73757e0a15e95123e310e102
hrmin <haminyang2@naver.com>
2025-01-23 22:47:35 +0900

    commit

---
+++ src/non01/Test05.java
@@ -0,0 +1,16 @@
+package non01;
+
+import java.util.Scanner;
+
+public class Test05 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a,b;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+}
