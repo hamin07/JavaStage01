c6b277aa87842a8e32089ce480815c14db36ebe4
hrmin <haminyang2@naver.com>
2025-01-29 21:48:23 +0900

    commit

--- src/non03/코딩은체육과목입니다.java
+++ src/non03/코딩은체육과목입니다.java
@@ -1,10 +1,23 @@
 package non03;
 
+import java.util.Scanner;
+
 public class 코딩은체육과목입니다 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+				
+		int n = sc.nextInt();
+		String str = "";
+		sc.close();
+				
+		for(int i=1; i<=n/4; i++) {
+			str += "long ";
+		}
+		System.out.println(str + "int");
 
 	}
-
+		
 }
+
+
