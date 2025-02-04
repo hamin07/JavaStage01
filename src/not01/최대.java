d12c68df011b4c9e4a0fb2a0ca2baee89b979b36
hrmin <haminyang2@naver.com>
2025-02-04 15:31:09 +0900

    commit

--- src/not01/최대.java
+++ src/not01/최대.java
@@ -1,5 +1,28 @@
 package not01;
 
-public class 최대 {
+import java.util.Scanner;
 
+public class 최대 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = { sc.nextInt(), sc.nextInt(), sc.nextInt(),
+				sc.nextInt(), sc.nextInt(), sc.nextInt(),
+				sc.nextInt(), sc.nextInt(), sc.nextInt() };
+		sc.close();
+		
+		int count = 0;
+		int max = 0;
+		int index = 0;
+        
+		for(int a : arr) {
+			count++;
+            
+			if(a > max) {
+				max = a;
+				index = count;
+			}
+		}
+		System.out.print(max + "\n" + index);
+		
+	}
 }
