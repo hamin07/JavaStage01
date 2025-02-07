0fe590bf234e10094cadeb7a70ac6774f4bf914e
hrmin <haminyang2@naver.com>
2025-02-07 23:17:35 +0900

    commit

---
+++ src/nin01/단어길이재기.java
@@ -0,0 +1,15 @@
+package nin01;
+
+import java.util.Scanner;
+
+public class 단어길이재기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String st = sc.next();
+		sc.close();
+		
+		System.out.println(st.length());
+		
+	}
+}
