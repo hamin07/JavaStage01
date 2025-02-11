50a7d237fba95123c4b11006c82cb6d34d4b5bb7
hrmin <haminyang2@naver.com>
2025-02-11 19:43:44 +0900

    commit

---
+++ src/nin01/상수.java
@@ -0,0 +1,19 @@
+package nin01;
+
+import java.util.Scanner;
+
+public class 상수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		sc.close();
+		
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.println(A > B ? A : B);
+	}
+}
